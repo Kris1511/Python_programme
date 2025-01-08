1a10419a423417617d2799e57bd150fa0d8a9775
Chandru-trigeminal <chandramohan@trigeminal-ai.com>
2025-01-08 16:44:53 +0530

    first commit

---
+++ Basics.py
@@ -0,0 +1,15 @@
+# Scripting way for printing Hello, world!
+print("Hello, world!")
+
+# Procedural way for printing Hello, world!
+def demo():
+    print("Inside procedural way of printing Hello, world!")
+demo()
+
+# Object Oriented way for printing Hello, world!
+class Demo:
+    def __init__(self):
+        print("Inside Object Oriented way of printing Hello, world!")
+
+obj = Demo()
+# obj.__init__()
---
+++ VariablesDataTypes/Overview.py
@@ -0,0 +1,3 @@
+# 1. Python is dynamically typed programming language.
+# 2. In python while declaring a variable we don't need to specify the data type of the variable.
+# 3. Data type will be assigned by language based on value stored into the variable.
