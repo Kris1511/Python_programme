de077d49a8fd468db480d925c2580ca29c22399f
Chandru-trigeminal <chandramohan@trigeminal-ai.com>
2025-01-21 13:20:01 +0530

    hacker rank question

---
+++ HackerRankProblem/Nested List/nestedList.py
@@ -0,0 +1,26 @@
+li = []
+
+n = int(input())
+
+for i in range(n):
+    name = input()
+    score = float(input())
+    li.append([name, score])
+score = []
+
+for name, score in li:
+    score.append(score)
+scores = list(set(score))
+
+score.set()
+
+name.li = []
+
+second_smallest.score = score[i] # type: ignore
+for name, score in li:
+    if score == second_smallest.score: # type: ignore
+        name_li.append(name) # type: ignore
+name_li.sort() # type: ignore
+
+for name in name.li:
+    print(name)
